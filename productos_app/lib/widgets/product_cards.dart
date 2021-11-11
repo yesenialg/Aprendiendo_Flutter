6250a74ef4de15545e30fe7bd23c8844cf6e1eca
Yesenia López Giraldo <yesenialopezg07@gmail.com>
2021-11-11 09:39:26 -0500

    Home de productos

---
+++ productos_app/lib/widgets/product_cards.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ProductCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Container(
+        margin: const EdgeInsets.only(top: 30, bottom: 50),
+        width: double.infinity,
+        height: 400,
+        color: Colors.indigo,
+      ),
+    );
+  }
+}
