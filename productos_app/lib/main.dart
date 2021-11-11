5052eadea54451c5c27aab1fec28d09b2ec3583a
Yesenia López Giraldo <yesenialopezg07@gmail.com>
2021-11-11 09:39:16 -0500

    Login

--- productos_app/lib/main.dart
+++ productos_app/lib/main.dart
@@ -10,14 +10,22 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Productos App',
-      initialRoute: 'login',
+      initialRoute: 'home',
       routes: {
-        'login': ( _ ) => LoginScreen(),
-        'home': ( _ ) => HomeScreen(),
+        'login': (_) => LoginScreen(),
+        'home': (_) => HomeScreen(),
       },
       theme: ThemeData.light().copyWith(
-        scaffoldBackgroundColor: Colors.grey[300]
-      )
+          scaffoldBackgroundColor: Colors.grey[300],
+          appBarTheme: AppBarTheme(
+            elevation: 0,
+            color: Colors.indigo,
+          ),
+          floatingActionButtonTheme: FloatingActionButtonThemeData(
+            backgroundColor: Colors.indigo,
+            elevation: 0,
+          )
+      ),
     );
   }
 }
--- productos_app/lib/screens/login_screen.dart
+++ productos_app/lib/screens/login_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/providers/login_form_provider.dart';
+import 'package:productos_app/ui/input_decorations.dart';
 import 'package:productos_app/widgets/widgets.dart';
+import 'package:provider/provider.dart';
 
 class LoginScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,21 +12,27 @@ class LoginScreen extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             children: [
-            SizedBox(height: 150,),
-            CardContainer(
-              child: Column(
-                children: [
-                  SizedBox(height: 10),
-                  Text('Ingreso', style: Theme.of(context).textTheme.headline4),
-                  SizedBox(height: 30),
-                  _LoginForm()
-
-                ],
+              SizedBox(
+                height: 150,
               ),
-            ),
-            SizedBox(height:50),
-            Text('Crea una nueva cuenta', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-          ],
+              CardContainer(
+                child: Column(
+                  children: [
+                    SizedBox(height: 10),
+                    Text('Ingreso',
+                        style: Theme.of(context).textTheme.headline4),
+                    SizedBox(height: 30),
+                    ChangeNotifierProvider(
+                        create: (_) => LoginFormProvider(),
+                        child: _LoginForm()),
+                  ],
+                ),
+              ),
+              SizedBox(height: 50),
+              Text('Crea una nueva cuenta',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              SizedBox(height: 30),
+            ],
           ),
         ),
       ),
@@ -33,35 +41,78 @@ class LoginScreen extends StatelessWidget {
 }
 
 class _LoginForm extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
+    final loginForm = Provider.of<LoginFormProvider>(context);
+
     return Container(
       child: Form(
+        key: loginForm.formKey,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
         child: Column(
           children: [
-            TextField(
+            TextFormField(
               autocorrect: false,
               keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.deepPurple
-                    )
-                  ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.deepPurple
-                    )
-                  ),
+              decoration: InputDecorations.authInputDecoration(
                   hintText: 'ejemplo@xxxx.com',
-                  labelText: 'Correo electrónico', 
-                  labelStyle: TextStyle(color: Colors.grey),
-                  prefixIcon: Icon(Icons.alternate_email_sharp, color: Colors.deepPurple)
-              )
+                  labelText: 'Correo electrónico',
+                  prefixIcon: Icons.alternate_email_rounded),
+              onChanged: (value) => loginForm.email = value,
+              validator: (value) {
+                String pattern =
+                    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                RegExp regExp = new RegExp(pattern);
+                return regExp.hasMatch(value ?? '')
+                    ? null
+                    : 'Ingrese un correo válido';
+              },
             ),
+            SizedBox(height: 30),
+            TextFormField(
+              autocorrect: false,
+              obscureText: true,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecorations.authInputDecoration(
+                  hintText: '********',
+                  labelText: 'Password',
+                  prefixIcon: Icons.alternate_email_rounded),
+              onChanged: (value) => loginForm.password = value,
+              validator: (value) {
+                return (value != null && value.length >= 8)
+                    ? null
+                    : 'La password debe ser mínimo de 8 caracteres';
+              },
+            ),
+            SizedBox(height: 30),
+            MaterialButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0)),
+              disabledColor: Colors.grey,
+              elevation: 0,
+              color: Colors.deepPurple,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                child: Text(
+                  loginForm.isLoading ? 'Espere' : 'Ingresar',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              onPressed: loginForm.isLoading ? null :() async {
+                FocusScope.of(context).unfocus();
+                if (!loginForm.isValidForm()) return;
+
+                loginForm.isLoading = true;
+
+                await Future.delayed(Duration(seconds: 2));
+                
+                loginForm.isLoading = true;
+
+                Navigator.pushReplacementNamed(context, 'home');
+              },
+            )
           ],
-          ),
+        ),
       ),
     );
   }
