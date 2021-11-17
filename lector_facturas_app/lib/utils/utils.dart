1449548271a36fbb8ca5f37e103e18c5010c3611
marianaph6 <mariana1996.mph@gmail.com>
2021-11-17 13:23:15 -0500

    -redireccionamiento a la url de la dian

--- lector_facturas_app/lib/utils/utils.dart
+++ lector_facturas_app/lib/utils/utils.dart
@@ -3,10 +3,19 @@ import 'package:lector_facturas_app/models/scan_model.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 launchInBrowser(BuildContext context, ScanModel scan) async {
-  final url = scan.valor;
-    if (await canLaunch(url)) {
-      await launch(url);
+  final valor = scan.valor;
+  const start = "CUFE=";
+  const end = "\n";
+
+  final startIndex = valor.indexOf(start);
+  final endIndex = valor.indexOf(end, startIndex + start.length);
+  final cufe= valor.substring(startIndex + start.length, endIndex);
+
+  final url_dian='https://catalogo-vpfe.dian.gov.co/Document/ShowDocumentToPublic/$cufe';
+
+     if (await canLaunch(url_dian)) {
+      await launch(url_dian);
     } else {
-      throw 'Could not launch $url';
-    }
+      throw 'Could not launch $url_dian';
+    } 
 }
