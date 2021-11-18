0b48a87ba60d6440b28688a34cebd7f6260f7e9f
Yesenia López Giraldo <yesenialopezg07@gmail.com>
2021-11-18 09:58:08 -0500

    Stateful en EnlacePage y alert al eliminar

--- lector_facturas_app/lib/pages/historial_enlaces.dart
+++ lector_facturas_app/lib/pages/historial_enlaces.dart
@@ -2,12 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:lector_facturas_app/providers/scan_list_provider.dart';
 import 'package:lector_facturas_app/utils/utils.dart';
 import 'package:provider/provider.dart';
+import 'package:art_sweetalert/art_sweetalert.dart';
 
-class EnlacesPage extends StatelessWidget {
+class EnlacesPage extends StatefulWidget {
+  @override
+  _EnlacesPageState createState() => _EnlacesPageState();
+}
+
+class _EnlacesPageState extends State<EnlacesPage> {
   @override
   Widget build(BuildContext context) {
-    final scanListProvider = Provider.of<ScanListProvider>(context);
-    final scans = scanListProvider.scans;
+    ScanListProvider scanListProvider = Provider.of<ScanListProvider>(context);
+    List scans = scanListProvider.scans;
 
     return ListView.builder(
       itemCount: scans.length,
@@ -16,30 +22,58 @@ class EnlacesPage extends StatelessWidget {
         background: Container(
           color: Colors.red,
         ),
-        onDismissed: (DismissDirection direcction) {
-          Provider.of<ScanListProvider>(context, listen: false)
-              .borrarScan(scans[i].id);
+        onDismissed: (DismissDirection direction) async {
+          ArtDialogResponse response = await ArtSweetAlert.show(
+              barrierDismissible: false,
+              context: context,
+              artDialogArgs: ArtDialogArgs(
+                denyButtonText: "Cancelar",
+                title: "¿Quieres eliminar el registro?",
+                confirmButtonText: "Eliminar",
+              ));
+
+          if (response.isTapConfirmButton) {
+            Provider.of<ScanListProvider>(context, listen: false)
+                .borrarScan(scans[i].id);
+            ArtSweetAlert.show(
+                context: context,
+                artDialogArgs: ArtDialogArgs(
+                    type: ArtSweetAlertType.success, title: "Eliminado!"));
+            return;
+          }
+
+          if (response.isTapDenyButton) {
+            scans = scanListProvider.scans;
+            setState(() {});
+            ArtSweetAlert.show(
+                context: context,
+                artDialogArgs: ArtDialogArgs(
+                    type: ArtSweetAlertType.info, title: "Cancelado"));
+            return;
+          }
         },
         child: Card(
           elevation: 10.0,
           shape:
-          RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
           child: Column(children: <Widget>[
             ListTile(
               leading: Icon(Icons.home, color: Theme.of(context).primaryColor),
-              title: Text('Nro Factura'+scans[i].num_factura ),
-              subtitle: Text(
-                'ID: '+scans[i].id.toString() +'\n'+
-                'FECHA: ' + scans[i].fecha +'\n'+
-                'Valor: ' + scans[i].total.toString())
-                ,
+              title: Text('Nro Factura' + scans[i].num_factura),
+              subtitle: Text('ID: ' +
+                  scans[i].id.toString() +
+                  '\n' +
+                  'FECHA: ' +
+                  scans[i].fecha +
+                  '\n' +
+                  'Valor: ' +
+                  scans[i].total.toString()),
               trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
               onTap: () {
                 launchInBrowser(context, scans[i]);
               },
             ),
-          ]
-        ),
+          ]),
         ),
       ),
     );
